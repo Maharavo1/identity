14d2d659c6e2d062fb52db6bd107bed171e02cd6
Maharavo1 <hei.maharavo@gmail.com>
2025-01-14 06:42:14 +0300

    feat(config): add Firebase initialization

---
+++ src/main/java/secu1/identity/configuration/FirebaseConfiguration.java
@@ -0,0 +1,33 @@
+package secu1.identity.configuration;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfiguration {
+
+    @PostConstruct
+    public void init() throws IOException {
+        String firebaseConfig = System.getenv("GOOGLE_SERVICES");
+
+        if (firebaseConfig == null || firebaseConfig.isEmpty()) {
+            throw new IllegalStateException(" vide !");
+        }
+
+        ByteArrayInputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes());
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+    }
+}
